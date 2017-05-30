695ea5580634ee82f54a5c24e6eaa5abe9ee8b69
Mike Su <kyatapi@gmail.com>
2017-05-30 21:09:53 +0800

    Add numeric support

--- phone_repeat.cpp
+++ phone_repeat.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <map>
 
 #ifndef ONLINE_JUDGE
 #include "test_cases.h"
@@ -9,8 +10,24 @@ inline void run_test(int argc, char **argv) {}
 
 using namespace std;
 
-unsigned int phone_to_digit(const string&) {
-	return 0;
+unsigned int phone_to_digit(const string& phone) {
+	const map<char, int> char_digit_mapping = {
+		{ '0', 0 }, 
+		{ '1', 1 }, { '2', 2 }, { '3', 3 },
+		{ '4', 4 }, { '5', 5 }, { '6', 6 },
+		{ '7', 7 }, { '8', 8 }, { '9', 9 }
+	};
+
+	unsigned int digits = 0;
+	for (const char p : phone)
+	{
+		try {
+			unsigned int digit = char_digit_mapping.at(p);
+			digits = digits * 10 + digit;
+		} catch (out_of_range e) {}
+	}
+
+	return digits;
 }
 
 int main(int argc, char **argv) {
--- test_cases.cpp
+++ test_cases.cpp
@@ -8,3 +8,9 @@ extern unsigned int phone_to_digit(const string&);
 TEST(PhoneToDigit, HandleEmptyPhone) {
 	ASSERT_EQ(0, phone_to_digit(""));
 }
+
+TEST(PhoneToDigit, HandleNumericPhone) {
+	ASSERT_EQ(2345678, phone_to_digit("2345678"));
+	ASSERT_EQ(5678, phone_to_digit("000-5678"));
+	ASSERT_EQ(301010, phone_to_digit("030-10-10"));
+}
