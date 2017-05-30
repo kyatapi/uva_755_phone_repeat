3fae7f00fd61222d099809741725dd180524de9e
Mike Su <kyatapi@gmail.com>
2017-05-31 00:58:54 +0800

    Use constant mapping instead of map to improve performance

--- phone_repeat.cpp
+++ phone_repeat.cpp
@@ -16,28 +16,32 @@ inline void run_test(int argc, char **argv) {}
 using namespace std;
 
 unsigned int phone_to_digit(const string& phone) {
-	static const map<char, int> char_digit_mapping = {
-		{ '0', 0 }, 
-		{ '1', 1 }, { '2', 2 }, { '3', 3 },
-		{ '4', 4 }, { '5', 5 }, { '6', 6 },
-		{ '7', 7 }, { '8', 8 }, { '9', 9 },
-		{ 'A', 2 }, { 'B', 2 }, { 'C', 2 },
-		{ 'D', 3 }, { 'E', 3 }, { 'F', 3 },
-		{ 'G', 4 }, { 'H', 4 }, { 'I', 4 },
-		{ 'J', 5 }, { 'K', 5 }, { 'L', 5 },
-		{ 'M', 6 }, { 'N', 6 }, { 'O', 6 },
-		{ 'P', 7 }, { 'R', 7 }, { 'S', 7 },
-		{ 'T', 8 }, { 'U', 8 }, { 'V', 8 },
-		{ 'W', 9 }, { 'X', 9 }, { 'Y', 9 },
+	static const unsigned int char_digit_mapping[] = {
+		2, 2, 2,
+		3, 3, 3,
+		4, 4, 4,
+		5, 5, 5,
+		6, 6, 6,
+		7, 0, 7, 7,
+		8, 8, 8,
+		9, 9, 9, 0
 	};
 
 	unsigned int digits = 0;
 	for (const char p : phone)
 	{
-		try {
-			unsigned int digit = char_digit_mapping.at(p);
-			digits = digits * 10 + digit;
-		} catch (out_of_range e) {}
+		unsigned int digit = 0;
+		if (p >= '0' &&  p <= '9') {
+			digit = p - '0';
+		}
+		else if (p >= 'A' && p <= 'Y' && p != 'Q') {
+			digit = char_digit_mapping[p - 'A'];	
+		}
+		else {
+			continue;
+		}
+
+		digits = digits * 10 + digit;
 	}
 
 	return digits;
