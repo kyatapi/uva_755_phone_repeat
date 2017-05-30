07620390ca8da65b251cb756f6c34ad2943f2f01
Mike Su <kyatapi@gmail.com>
2017-05-30 21:19:05 +0800

    Add alphabetic support

--- test_cases.cpp
+++ test_cases.cpp
@@ -14,3 +14,9 @@ TEST(PhoneToDigit, HandleNumericPhone) {
 	ASSERT_EQ(5678, phone_to_digit("000-5678"));
 	ASSERT_EQ(301010, phone_to_digit("030-10-10"));
 }
+
+TEST(PhoneToDigit, HandleAlphabeticPhone) {
+	ASSERT_EQ(4873279, phone_to_digit("ITS-EASY"));
+	ASSERT_EQ(4875566, phone_to_digit("HUP-KLON"));
+	ASSERT_EQ(6666345, phone_to_digit("NO-NO-DHL"));
+}
